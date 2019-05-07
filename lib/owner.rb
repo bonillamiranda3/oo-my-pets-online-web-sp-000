1b1ca16eade56ea17f0b48dc7a775f3ed5dc31e6
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-07 01:00:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,50 +1,56 @@
 class Owner
-  # code goes here
-    attr_accessor :name, :pets
-    attr_reader :species
-    @@all = []
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+  @@count = 0
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@count += 1
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats=> []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
     @@count = 0
+  end
+  def self.count
+    @@count
+  end
 
-    def initialize(name)
-      @name = name
-      @@all << self
-      @@count +=1
-      @species = "human"
-      @pets = {:fishes => [], :dogs => [], :cats => []}
-    end
-    def self.all
-      @@all
-    end
+  def say_species
+    "I am a #{@species}."
+  end
 
-    def say_species
-      "I am a #{@species}."
-    end
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
 
-    def buy_fish(name)
-      new_fish = Fish.new(name)
-      @pets[:fishes] << new_cat
-    end
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cats] << new_cat
+  end
 
-    def buy_dog(name)
-      new_dog = Dog.new(name)
-      @pets[:dogs] << new_dog
-    end
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
 
-    def buy_cat(name)
-      new_cat = Cat.new(name)
-      @pets[:cats] << new_cat
-    end
-
-    def walk_dogs
-      @pets[:dogs].each do |dog|
+  def walk_dogs
+    @pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
 
     def play_with_cats
       @pets[:cats].each do |cat|
-      cat.mood = "happy"
+        cat.mood = "happy"
+      end
     end
-  end
 
     def feed_fish
       @pets[:fishes].each do |fish|
@@ -69,4 +75,4 @@ class Owner
     "I have #{fish_count} fish, #{dog_count} dog(s), and #{cat_count} cat(s)."
   end
 
-end
+end 
